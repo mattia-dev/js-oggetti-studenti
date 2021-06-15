785202c46e95bde780f774f75ea0d81f00a0e358
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-06-15 16:40:20 +0200

    created html and js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// PARTE 1
+// Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell’oggetto.
+
+// PARTE 2
+// Creare un array di oggetti di studenti.Ciclare su tutti gli studenti e stampare per ognuno di essi,
+// nome e cognome.Ricordatevi, è un array, quindi ? for.
+
+// PARTE 3
+// Dare la possibilità all’utente, attraverso 3 prompt() - uno per ciascuna proprietà - di aggiungere
+// UN SOLO oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+    
+
+})
