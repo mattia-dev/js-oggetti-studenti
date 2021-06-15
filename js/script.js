969ff779f9e3929e2d6cee47f4cb7f252a34ca93
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-06-15 17:40:07 +0200

    part 2 done

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell’oggetto.
 
 // PARTE 2
-// Creare un array di oggetti di studenti.Ciclare su tutti gli studenti e stampare per ognuno di essi,
+// Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno di essi,
 // nome e cognome.Ricordatevi, è un array, quindi ? for.
 
 // PARTE 3
@@ -31,6 +31,41 @@ document.addEventListener('DOMContentLoaded', function () {
         
         studentHtmlElement.innerHTML += objectCaseAdjusted(key) + ": " + student[key] + "<br>";
             
+    }
+
+    // PARTE 2
+    var students = [
+
+        student1 = {
+            name: "Mattia",
+            surname: "Ferrari",
+            age: "24"
+        },
+
+        student2 = {
+            name: "Pinco",
+            surname: "Pallo",
+            age: "21"
+        },
+
+        student3 = {
+            name: "Mario",
+            surname: "Rossi",
+            age: "31"
+        }
+
+    ];
+
+    var studentsHtmlElement = document.getElementById('students');
+
+    for (var i = 0; i < students.length; i++) {
+
+        for (const chiave in students[i]) {
+
+            studentsHtmlElement.innerHTML += objectCaseAdjusted(chiave) + ": " + students[i][chiave] + "<br>";
+
+        }  
+        
     }
 
 })
